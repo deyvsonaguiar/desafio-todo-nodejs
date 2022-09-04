b5ece625174b88c3c867b3334b93c004d92c9e37
Deyvson Aguiar <deyvsonaguiar@gmail.com>
2022-09-03 22:40:48 -0400

    Create a new User

--- src/index.js
+++ src/index.js
@@ -1,21 +1,42 @@
 const express = require('express');
 const cors = require('cors');
 
-// const { v4: uuidv4 } = require('uuid');
+const { v4: uuid } = require('uuid');
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
+app.listen(3000)
 
-// const users = [];
+const users = [];
 
 function checksExistsUserAccount(request, response, next) {
   // Complete aqui
 }
 
 app.post('/users', (request, response) => {
-  // Complete aqui
+  
+  const { name, username } = request.body
+
+  const ifAlreadyExists = users.some(
+    users => users.name === name | users.username === username
+  )
+
+  if(ifAlreadyExists) {
+    return response.status(400).json({error: "User already exists"})
+  }
+
+  users.push({
+    id: uuid(),
+    name, 
+    username,
+    todos: []
+  })
+
+  return response.status(201).json(users)
+
+
 });
 
 app.get('/todos', checksExistsUserAccount, (request, response) => {
